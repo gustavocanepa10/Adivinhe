8eb834c0ebd7c1ab130fa59b91459cce70d2b949
gustavocanepa10 <gustavociscotto@gmail.com>
2025-01-14 10:59:30 -0300

    Header finalizado

--- guessing-game/src/App.tsx
+++ guessing-game/src/App.tsx
@@ -4,10 +4,14 @@ import styles from "./app.module.css"
 
 
 function App() {
+
+  function handleRestartGame() {
+    alert("Reiniciar o jogo")
+  }
   return (
     <div className={styles.container}>
       <main>
-        <Header/>
+        <Header current={5} max={10} onRestart={handleRestartGame}/>
         </main>
       
       
--- guessing-game/src/components/Header/index.tsx
+++ guessing-game/src/components/Header/index.tsx
@@ -1,11 +1,26 @@
 import Logo from "../../assets/logo.png"
 import styles from "./style.module.css"
+import Restart from "../../assets/restart.svg"
 
 
-export function Header() {
+type Props = {
+    current: number,
+    max: number,
+    onRestart : () => void
+}
+
+export function Header({current,max, onRestart}: Props) {
     return (
         <div className={styles.container} >
             <img src={Logo} alt="Logo"/>
+            <header>
+                <span>
+                    <strong>{current}</strong> de {max} tentativas
+                </span>
+                <button type="button" onClick={onRestart}>
+                    <img src={Restart} alt="Icone de reiniciar" />
+                </button>
+            </header>
         </div>
     )
 
