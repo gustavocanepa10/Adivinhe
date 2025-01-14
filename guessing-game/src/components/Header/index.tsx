8152bba42be6a30a6df8aaf86c5177c5fb23d635
gustavocanepa10 <gustavociscotto@gmail.com>
2025-01-14 10:46:27 -0300

    Adicionando os primeiros estilos

---
+++ guessing-game/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Logo from "../../assets/logo.png"
+import styles from "./style.module.css"
+
+
+export function Header() {
+    return (
+        <div className={styles.container} >
+            <img src={Logo} alt="Logo"/>
+        </div>
+    )
+
+}
+
