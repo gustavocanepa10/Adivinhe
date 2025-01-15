20c7b52096399f6bb338b000d8632f347c038fc5
João Gustavo <gustavociscotto@gmail.com>
2025-01-15 16:08:28 -0300

    Começando as funcionalidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Header } from './components/Header';
+import { Challenge, WORDS } from './utils/words';
+import { useEffect , useState } from 'react';
 import styles from "./app.module.css";
 import { Tip } from './components/Tip';
 import { Letter } from './components/Letter';
@@ -7,14 +9,32 @@ import { Button } from './components/Button';
 import {LettersUsed} from "./components/LettersUsed"
 
 function App() {
+  const [attempts, setAttempts] = useState(0)
+  const [letter, setLetter] = useState("")
+  const [challenge, setChallenge] = useState<Challenge | null> (null)
+
+
   function handleOnRestart() {
     alert("Jogo Reiniciando...");
   }
 
+  function StartGame() {
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    StartGame()
+  } , [])
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} OnRestart={handleOnRestart} />
+        <Header current={attempts} max={10} OnRestart={handleOnRestart} />
 
         <Tip tip="Uma das linguagens de programação mais populares" />
 
