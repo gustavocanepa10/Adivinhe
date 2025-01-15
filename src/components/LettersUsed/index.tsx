71c585ad331d25b87cdb3daa03a74d255a6278ca
gustavocanepa10 <gustavociscotto@gmail.com>
2025-01-15 11:22:21 -0300

    Interface Finalizada

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,10 +1,20 @@
 import styles from "./styles.module.css"
 
 
+import { Letter } from "../Letter"
+
+
 export function LettersUsed() {
     return (
         <div className={styles.lettersUsed}>
             <h5>Letras Utilizadas</h5>
+
+            <div>
+                <Letter value="r"  size="small" color="correct"/>
+                <Letter value="X"  size="small" color="wrong"/>
+               
+
+            </div>
 
         </div>
     )
